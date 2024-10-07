7a473773af7098c1e0f0bd2eadd37e0a6f462787
oumondad <oumondad@c4r9p2.1337.ma>
2024-10-07 18:55:39 +0100

    print initialisation

--- alfalasifa/philosophers.c
+++ alfalasifa/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: oumondad <oumondad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:16:01 by oumondad          #+#    #+#             */
-/*   Updated: 2024/09/17 20:10:31 by oumondad         ###   ########.fr       */
+/*   Updated: 2024/09/29 17:26:50 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,11 @@ int	main(int ac, char **av)
 	if (ac >= 5 && ac <= 6)
 	{
 		initialisation(&data, av, ac);
+		printf("nop -> %ld\n", data.nop);
+		printf("ttd -> %ld\n", data.ttd);
+		printf("tte -> %ld\n", data.tte);
+		printf("tts -> %ld\n", data.tts);
+		printf("nom -> %ld\n", data.nom);
 		if (data.nop == 0 || data.nom == 0)
 			return (0);
 	}
